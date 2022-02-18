6596bb9906c17ed055315681b13b7e0c1ae4d138
fffadedev <fffadedev@gmail.com>
2022-02-18 12:02:47 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const fs = require('fs');
+/*
+    File: index.js
+    Author: Fade
+    Date: 2022-02-18
 
-console.log(`Writing to log...`);
+    The main file to run the central server and all its components.
+*/
 
-fs.writeFileSync('logs.txt', "This is a log");
+const express = require('express');
+const env = require('dotenv');
 
-console.log(`Complete`);
+env.config({path: './.env'}); // load environment variables
+
+const app = express();
+
+app.use(express.static('./view/')); // deploy html in the view folder
+
+// start the application at the defined port in .env
+app.listen(process.env.PORT, () => {
+
+    console.log(`Server running on port ${process.env.PORT}`);
+});
